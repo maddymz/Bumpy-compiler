8c867dc2acc6d0aa70ecb422080fa3c7cefac20f
unknown <pchugh1@asu.edu>
2019-04-16 00:34:34 -0700

    Parser of the language modified

--- src/compiler/parser.pl
+++ src/compiler/parser.pl
@@ -1,38 +1,38 @@
-program(t_program(X,Y)) -->block(t_block(X));comment(t_comment(Y)).
-program(t_program(X)) -->block(t_block(X)).
+program(t_program(X,Y)) -->comment(X),block(Y).
+program(t_program(X)) -->block(X).
 
-comment(t_comment(Y)) --> [@],words(t_words(Y)),[@].
+comment(t_comment(Y)) --> [@],words(Y),[@].
 
-words(t_words(X,Y)) --> identifier(t_identifier(X)), words(t_words(Y)) ; numb(t_numb(X)), words(t_words(Y)).
-words(t_words(X)) -->identifier(t_identifier(X)); numb(t_numb(X)).
+words(t_words(X,Y)) --> identifier(X), words(Y) ; numb(X), words(Y).
+words(t_words(X)) -->identifier(X); numb(X).
 
-block(t_block(X,Y)) --> [start], declaration(t_declare(X)), process(t_process(Y)), [end].
+block(t_block(X,Y)) --> [start], declaration(X), process(Y), [end].
 
-declaration(t_declare(X,Y)) --> [var], identifier(t_identifier(X)), [;], declaration(t_declare(Y)) ; [bool], identifier(t_identifier(X)), [;] ,declaration(t_declare(Y)).    
-declaration(t_declare(X)) -->[var], identifier(t_identifier(X)); [bool], identifier(t_identifier(X)).
+declaration(t_declare(X,Y)) --> [var], identifier(X), [;], declaration(Y) ; [bool], identifier(X), [;] ,declaration(Y).    
+declaration(t_declare(X)) -->[var], identifier(X); [bool], identifier(X).
 
-process(t_process(X,Y)) --> assignvalue(t_assign(X)), [;], process(t_process(Y)); control(t_control(X)), [;], process(t_process(Y)); iterate(t_iterate(X)), [;], process(t_process(Y)).
-process(t_process(X)) -->assignvalue(t_assign(X)) ;control(t_control(X)) ;iterate(t_iterate(X)).
+process(t_process(X,Y)) --> assignvalue(X), [;], process(Y); control(X), [;], process(Y); iterate(X), [;], process(Y).
+process(t_process(X)) -->assignvalue(X) ;control(X) ;iterate(X).
 
-assignvalue(t_assign(X,Y)) --> identifier(t_identifier(X)), [=] ,expression(t_expr(Y)); identifier(t_identifier(X)), [is], boolexp(t_boolexp(Y)).
+assignvalue(t_assign(X,Y)) --> identifier(X), [=] ,expression(Y); identifier(X), [is], boolexp(Y).
 
-control(t_control(X,Y,Z)) --> [incase], condition(t_cond(X)), [do], process(t_process(Y)), [otherwise], process(t_process(Z)), [endcase].
+control(t_control(X,Y,Z)) --> [incase], condition(X), [do], process(Y), [otherwise], process(Z), [endcase].
 
-iterate(t_iterate(X,Y)) --> [when], condition(t_cond(X)), [repeat], process(t_process(Y)), [endrepeat].
+iterate(t_iterate(X,Y)) --> [when], condition(X), [repeat], process(Y), [endrepeat].
 
-condition(t_cond(X,Y)) --> boolexp(t_boolexp(X)), [and], boolexp(t_boolexp(Y)); boolexp(t_boolexp(X)), [or], boolexp(t_boolexp(Y)).
-condition(t_cond(X)) -->[~], boolexp(t_boolexp(X)); boolexp(t_boolexp(X)).
+condition(t_cond(X,Y)) --> boolexp(X), [and], boolexp(Y); boolexp(X), [or], boolexp(Y).
+condition(t_cond(X)) -->[~], boolexp(X); boolexp(X).
 
-boolexp(t_boolexp(X,Y)) --> expression(t_expr(X)), [:=:], expression(t_expr(Y)); expression(t_expr(X)), [~=], expression(t_expr(Y)); 
-    expression(t_expr(X)), [<=], expression(t_expr(Y)); expression(t_expr(X)), [>=], expression(t_expr(Y)); expression(t_expr(X)), [<], expression(t_expr(Y));
-    expression(t_expr(X)), [>], expression(t_expr(Y)); [yes]; [no].
+boolexp(t_boolexp(X,Y)) --> expression(X), [:=:], expression(Y); expression(X), [~=], expression(Y); 
+    expression(X), [<=], expression(Y); expression(X), [>=], expression(Y); expression(X), [<], expression(Y);
+    expression(X), [>], expression(Y); [yes]; [no].
 
-expression(t_expr(X,Y)) --> term(t_term(X)),[+],expression(t_expr(Y)); term(t_term(X)),[-],expression(t_expr(Y)).
-expression(t_expr(X)) -->term(t_term(X)).
+expression(t_expr(X,Y)) --> term(X),[+],expression(Y); term(X),[-],expression(Y).
+expression(t_expr(X)) -->term(X).
 
-term(t_term(X,Y)) --> identifier(t_identifier(X)),[*],term(t_term(Y)); numb(t_numb(X)),[*],term(t_term(Y)); identifier(t_identifier(X)),[/],term(t_term(Y)); numb(t_numb(X)),[/],term(t_term(Y));
-    identifier(t_identifier(X)),[mod],term(t_term(Y)); numb(t_numb(X)),[mod],term(t_term(Y)).
-term(t_term(X)) -->identifier(t_identifier(X)); numb(t_numb(X)).
+term(t_term(X,Y)) --> identifier(X),[*],term(Y); numb(X),[*],term(Y); identifier(X),[/],term(Y); numb(X),[/],term(Y);
+    identifier(X),[mod],term(Y); numb(X),[mod],term(Y).
+term(t_term(X)) -->identifier(X); numb(X).
 
 %identifier --> [a],identifier;[b],identifier;[c],identifier;[d],identifier;[e],identifier;[f],identifier;[g],identifier;[h],identifier;[i],identifier;[j],identifier;[k],identifier;[l],identifier;[m],identifier;[n],identifier;[o],identifier;[p],identifier;[q],identifier;[r],identifier;[s],identifier;[t],identifier;[u],identifier;[v],identifier;[w],identifier;[x],identifier;[y],identifier;[z],identifier.
 identifier(t_identifier(a)) -->[a].
@@ -62,10 +62,10 @@ identifier(t_identifier(x)) -->[x].
 identifier(t_identifier(y)) -->[y].
 identifier(t_identifier(z)) -->[z].
 
-numb(t_numb(X)) --> digit(t_digit(X)).
-numb(t_numb(X,Y)) -->digit(t_digit(X)), numb(t_numb(Y)).
-numb(t_numb(X,Y,Z)) -->digit(t_digit(X)), numb(t_numb(Y)),[.],digit(t_digit(Z)).
-numb(t_numb(X,Y)) -->digit(t_digit(X)),[.],digit(t_digit(Y)).
+numb(t_numb(X)) --> digit(X).
+numb(t_numb(X,Y)) -->digit(X), numb(Y).
+numb(t_numb(X,Y,Z)) -->digit(X), numb(Y),[.],digit(Z).
+numb(t_numb(X,Y)) -->digit(X),[.],digit(Y).
 
 digit(t_digit(0)) -->[0].
 digit(t_digit(1)) -->[1].
