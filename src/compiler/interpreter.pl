b906106d90e11fe5e4c97c3c9efa228b8df6b0d8
unknown <pchugh1@asu.edu>
2019-04-22 22:52:37 -0700

    Added Rules for Program and comments

--- src/compiler/interpreter.pl
+++ src/compiler/interpreter.pl
@@ -1,10 +1,26 @@
-% Rules to evaluate expressions.
+evalProgram(t_program(X, Y), EnvIn, EnvOut) :-evalComment(X, EnvIn, EnvIn2),evalBlock(Y, EnvIn2, EnvOut),!.
+evalProgram(t_program(X), EnvIn, EnvOut) :- evalBlock(X, E0, E).
 
+evalComment(t_comment(X),EnvIn,EnvOut) :-evalWords(X,E0,E).
+
+evalWords(t_words(X,Y),EnvIn,EnvOut) :-evalIdentifier(X,EnvIn,EnvIn2),evalWords(Y,EnvIn2,EnvOut),!.
+
+
+
+% Rules for declaration.
 evalDeclaration(t_declare(X, Y, Z), EnvIn, EnvOut) :- evalDatatype(X, EnvIn, EnvIn1), evalIdentifier(Y, _, IdName, EnvIn1, EnvIn2), 
 evalDeclaration(Z, EnvIn2, EnvIn3), update(IdName, 0, EnvIn3, EnvOut).
-
 evalDeclaration(t_declare(X, Y), EnvIn, EnvOut) :- evalDatatype(X, EnvIn, EnvIn1),
 evalIdentifier(Y, _, IdName, EnvIn1, EnvIn2), update(IdName, 0, EnvIn2, EnvOut).
+
+evalNum(t_num(X), Output, EnvIn, EnvIn) :- Output = X, !.
+evalNumneg(t_numneg(X), Output, EnvIn, EnvIn) :- Output = X, !.
+evalIdentifier(t_identifier(X), Output, Ident, EnvIn, EnvIn) :- lookup(X, EnvIn, Output), Ident = X.
+
+evalDatatype(t_datatype(var), EnvIn, EnvIn).
+evalDatatype(t_datatype(bool), EnvIn, EnvIn).
+
+% Rules to evaluate expressions.
 
 evalExpression(t_add(X,Y), Output, EnvIn, EnvOut) :- evalTerm(X, TermOut, EnvIn, EnvIn2),
                                                      evalExpression(Y, ExpOut, EnvIn2, EnvOut),
@@ -31,6 +47,7 @@ evalTerm(t_mul(X,Y), Output, EnvIn, EnvOut) :- evalFactor(X, FactOut, EnvIn, Env
                            atom_string(FactOut, QfactOut),
                            atom_number(QfactOut, NfactOut),
                                                Output is NfactOut * NtermOut.
+
 evalTerm(t_div(X,Y), Output, EnvIn, EnvOut) :- evalFactor(X, FactOut, EnvIn, EnvIn2),
                                                evalTerm(Y, TermOut, EnvIn2, EnvOut),
                            atom_string(TermOut, QtermOut),
@@ -38,6 +55,7 @@ evalTerm(t_div(X,Y), Output, EnvIn, EnvOut) :- evalFactor(X, FactOut, EnvIn, Env
                             atom_string(FactOut, QfactOut),
                             atom_number(QfactOut, NfactOut),
                                                Output is NfactOut / NtermOut.
+
 evalTerm(t_mod(X,Y), Output, EnvIn, EnvOut) :- evalFactor(X, FactOut, EnvIn, EnvIn2),
                                                evalTerm(Y, TermOut, EnvIn2, EnvOut),
                            atom_string(TermOut, QtermOut),
@@ -45,15 +63,9 @@ evalTerm(t_mod(X,Y), Output, EnvIn, EnvOut) :- evalFactor(X, FactOut, EnvIn, Env
                             atom_string(FactOut, QfactOut),
                             atom_number(QfactOut, NfactOut),
                                                Output is NfactOut mod NtermOut.
-evalTerm(t_term(X), Output, EnvIn, EnvOut) :- evalFactor(X, Output, EnvIn, EnvOut).
 
-evalFactor(t_num(X), Output, EnvIn, EnvOut) :- evalNum(X, Output, EnvIn, EnvOut).
-evalFactor(t_numneg(X), Output, EnvIn, EnvOut) :- evalNumneg(X, Output, EnvIn, EnvOut).
-evalFactor(t_identifier(X), Output, EnvIn, EnvOut) :- evalIdentifier(X, Output, EnvIn, EnvOut).
+evalTerm(t_term(X), Output, EnvIn, EnvOut) :- evalNum(X, Output, EnvIn, EnvOut).
+evalTerm(t_term(X), Output, EnvIn, EnvOut) :- evalNumneg(X, Output, EnvIn, EnvOut).
+evalTerm(t_term(X), Output, EnvIn, EnvOut) :- evalIdentifier(X, Output, EnvIn, EnvOut).
 
-evalNum(t_num(X), Output, EnvIn, EnvIn) :- Output = X, !.
-evalNumneg(t_numneg(X), Output, EnvIn, EnvIn) :- Output = X, !.
-evalIdentifier(t_identifier(X), Output, Ident, EnvIn, EnvIn) :- lookup(X, EnvIn, Output), Ident = X.
 
-evalDatatype(t_datatype(var), EnvIn, EnvIn).
-evalDatatype(t_datatype(bool), EnvIn, EnvIn).
