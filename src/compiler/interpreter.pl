8436a0d62bb1fd7645df0a149538339cc31c3f10
unknown <pchugh1@asu.edu>
2019-04-26 23:49:13 -0700

    Recreated the Interpreter with fixes

--- src/compiler/interpreter.pl
+++ src/compiler/interpreter.pl
@@ -1,104 +1,238 @@
-evalProgram(t_program(X, Y), EnvIn, EnvOut) :-evalComment(X, EnvIn, EnvIn2),evalBlock(Y, EnvIn2, EnvOut),!.
-evalProgram(t_program(X), EnvIn, EnvOut) :- evalBlock(X, EnvIn, EnvOut).
+evalProgram(t_program(X),EnvOut) :- evalBlock(X,[], EnvOut).
 
-evalComment(t_comment(X),EnvIn,EnvOut) :-evalWords(X,EnvIn,EnvOut).
+evalProcess(t_process(X),EnvIn,EnvOut):-evalAssign(X,EnvIn,EnvOut).
+evalProcess(t_process(X),EnvIn,EnvOut):-evalIterate(X,EnvIn,EnvOut).
+evalProcess(t_process(X),EnvIn,EnvOut):-evalControl(X,EnvIn,EnvOut).
+evalProcess(t_process(X,Y),EnvIn,EnvOut):-evalAssign(X,EnvIn,EnvIn2),
+    evalProcess(Y,EnvIn2,EnvOut).
+evalProcess(t_process(X,Y),EnvIn,EnvOut):-evalIterate(X,EnvIn,EnvIn2),
+    evalProcess(Y,EnvIn2,EnvOut).
+evalProcess(t_process(X,Y),EnvIn,EnvOut):-evalControl(X,EnvIn,EnvIn2),
+    evalProcess(Y,EnvIn2,EnvOut).
 
-evalWords(t_words(X,Y),EnvIn,EnvOut) :-evalIdentifier(X,EnvIn,EnvIn2),evalWords(Y,EnvIn2,EnvOut),!.
+% Look up the environment to find the value of a variable
+lookup(_,[],0).
+lookup(X,[(X, V)|_],V).
+lookup(X,[_|T],V) :- lookup(X,T,V).
 
-evalBlock(t_block(X,Y),EnvIn,EnvOut) :-evalDeclaration(X,EnvIn, EnvIn2),evalProcess(Y,EnvIn2,EnvOut),!.
+% Update the env with new values of variables
+update(X,V,[],[(X,V)]).
+update(X,V,[(X,_)|T],[(X,V)|T]).
+update(X,V,[H|T],[H|T1]) :- update(X,V,T,T1).
 
-evalProcess(t_process(X,Y),EnvIn,EnvOut):-evalAssign(X,EnvIn, EnvIn2),evalProcess(Y,EnvIn2,EnvOut).
-evalProcess(t_process(X,Y),EnvIn,EnvOut):-evalControl(X,EnvIn, EnvIn2),evalProcess(Y,EnvIn2,EnvOut).
-evalProcess(t_process(X,Y),EnvIn,EnvOut):-evalIterate(X,EnvIn, EnvIn2),evalProcess(Y,EnvIn2,EnvOut).
+% iterate
+%evalIterate(t_iterate(X,Y),EnvIn,EnvOut):-
+ %    evalCond(X,Output,EnvIn,EnvIn),
+  %   Output = true,
+   %  evalProcess(Y,EnvIn,EnvIn2),
+    % evalIterate(t_iterate(X,Y),EnvIn2,EnvOut).
 
-evalProcess(t_process(X),EnvIn,EnvOut):-evalAssign(X,EnvIn, EnvOut).
-evalProcess(t_process(X),EnvIn,EnvOut):-evalControl(X,EnvIn, EnvOut).
-evalProcess(t_process(X),EnvIn,EnvOut):-evalIterate(X,EnvIn, EnvOut).
+%evalIterate(t_iterate(X,_),EnvIn,_):-
+ %    evalCond(X,Output,EnvIn,EnvIn),
+  %   Output = false.
+
+evalIterate(t_iterate(X,Y),EnvIn,EnvOut):- (evalCond(X,Output,EnvIn, EnvIn),Output=true ->  evalProcess(Y, EnvIn, EnvIn2),evalIterate(t_iterate(X,Y), EnvIn2,EnvOut));EnvOut = EnvIn.
+
+%Control
+evalControl(t_control(X,Y,_),EnvIn,EnvOut):-
+    evalCond(X,Output,EnvIn,EnvIn),
+    Output = true,
+    evalProcess(Y,EnvIn,EnvOut).
+
+evalControl(t_control(X,_,Z),EnvIn,EnvOut):-
+    evalCond(X,Output,EnvIn,EnvIn),
+    Output = false,
+    evalProcess(Z,EnvIn,EnvOut).
+
+%Condition
+evalCond(t_cond(X),Output,EnvIn,EnvIn):-evalBoolexp(X,Output,EnvIn,EnvIn),!.
+evalCond(t_cond_not(X),Output,EnvIn,EnvIn):-
+    evalBoolexp(X,BoolOutput,EnvIn,EnvIn),
+    BoolOutput = true,
+    Output = false,!.
+
+evalCond(t_cond_not(X),Output,EnvIn,EnvIn):-
+    evalBoolexp(X,BoolOutput,EnvIn,EnvIn),
+    BoolOutput = false,
+    Output = true,!.
+
+evalCond(t_cond_and(X,Y),Output,EnvIn,EnvIn):-
+    evalBoolexp(X,BoolOutput1,EnvIn,EnvIn),
+    evalBoolexp(Y,BoolOutput2,EnvIn,EnvIn),
+    BoolOutput1 = true,
+    BoolOutput2 = true,
+    Output = true; Output = false,!.
+
+evalCond(t_cond_or(X,Y),Output,EnvIn,EnvIn):-
+    evalBoolexp(X,BoolOutput1,EnvIn,EnvIn),
+    evalBoolexp(Y,BoolOutput2,EnvIn,EnvIn),
+    BoolOutput1 = true,
+    BoolOutput2 = false,
+    Output = true,!.
+
+evalCond(t_cond_or(X,Y),Output,EnvIn,EnvIn):-
+    evalBoolexp(X,BoolOutput1,EnvIn,EnvIn),
+    evalBoolexp(Y,BoolOutput2,EnvIn,EnvIn),
+    BoolOutput2 = true,
+    BoolOutput1 = false,
+    Output = true,!.
+
+evalCond(t_cond_or(X,Y),Output,EnvIn,EnvIn):-
+    evalBoolexp(X,BoolOutput1,EnvIn,EnvIn),
+    evalBoolexp(Y,BoolOutput2,EnvIn,EnvIn),
+    BoolOutput1 = true,
+    BoolOutput2 = true,
+    Output = true,!.
+
+evalCond(t_cond_or(X,Y),Output,EnvIn,EnvIn):-
+    evalBoolexp(X,BoolOutput1,EnvIn,EnvIn),
+    evalBoolexp(Y,BoolOutput2,EnvIn,EnvIn),
+    BoolOutput1 = false,
+    BoolOutput2 = false,
+    Output = false,!.
+    
+    						
+
+%Block
+evalBlock(t_block(X,Y),EnvIn,EnvOut) :-
+    evalDeclaration(X,EnvIn,EnvIn2),
+    evalProcess(Y,EnvIn2,EnvOut).
+
+%datatype
+evalDatatype(t_datatype(var),Output,EnvIn,EnvIn):-Output=var,!.
+evalDatatype(t_datatype(bool),Output,EnvIn,EnvIn):-Output=bool,!.
+
+%bool expression
+evalBoolexp(t_boolexp(yes),true,EnvIn,EnvIn).
+evalBoolexp(t_boolexp(no),false,EnvIn,EnvIn).
+evalBoolexp(t_boolexp_eq(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalExpression(Y,Output2,EnvIn,EnvIn),
+    		Output1 =:= Output2, Output = true; Output = false,!.
+
+evalBoolexp(t_boolexp_neq(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalExpression(Y,Output2,EnvIn,EnvIn),
+    		Output1 =\= Output2, Output = true; Output = false,!.
+
+evalBoolexp(t_boolexp_geq(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalExpression(Y,Output2,EnvIn,EnvIn),
+    		Output1 >= Output2, Output = true; Output = false,!.
+
+evalBoolexp(t_boolexp_leq(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalExpression(Y,Output2,EnvIn,EnvIn),
+    		Output1 =< Output2, Output = true; Output = false,!.
+
+evalBoolexp(t_boolexp_less(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalExpression(Y,Output2,EnvIn,EnvIn),
+    		Output1 < Output2, Output = true; Output = false,!.
+
+evalBoolexp(t_boolexp_great(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalExpression(Y,Output2,EnvIn,EnvIn),
+    		Output1 > Output2, Output = true; Output = false,!.
+
+evalBoolexp(t_boolexp_beq(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalBoolexp(Y,Output2,EnvIn,EnvIn),
+    		Output1 =:= Output2, Output = true; Output = false,!.
+
+evalBoolexp(t_boolexp_bneq(X,Y),Output,EnvIn,EnvIn):-
+    		evalExpression(X,Output1,EnvIn,EnvIn),
+    	    evalBoolexp(Y,Output2,EnvIn,EnvIn),
+    		Output1 =\= Output2, Output = true; Output = false,!.
 
 
-% Rules for declaration.
-evalDeclaration(t_declare(X, Y, Z), EnvIn, EnvOut) :- evalDatatype(X, EnvIn, EnvIn1), evalIdentifier(Y, _, IdName, EnvIn1, EnvIn2), 
-evalDeclaration(Z, EnvIn2, EnvIn3), update(IdName, 0, EnvIn3, EnvOut).
-evalDeclaration(t_declare(X, Y), EnvIn, EnvOut) :- evalDatatype(X, EnvIn, EnvIn1),
-evalIdentifier(Y, _, IdName, EnvIn1, EnvIn2), update(IdName, 0, EnvIn2, EnvOut).
+%declaration
+evalDeclaration(t_declare(X,Y),EnvIn,EnvOut) :-
+    evalDatatype(X,_,EnvIn,EnvIn),
+    evalIdentifier(Y,_,EnvIn,EnvIn),update(Y,0, EnvIn, EnvOut).
 
-evalAssign(t_assign(X,Y), EnvIn, EnvOut) :- 
-    evalIdentifier(X, _, IdName, EnvIn, EnvIn2),
-    evalExpression(Y, Output, EnvIn2, EnvIn3),
-    update(IdName, Output, EnvIn3, EnvOut).
+evalDeclaration(t_declare(X,Y,Z),EnvIn,EnvOut) :-
+    evalDatatype(X,_,EnvIn,EnvIn),
+    evalIdentifier(Y,_,EnvIn,EnvIn),update(Y,0, EnvIn, EnvIn2),
+    evalDeclaration(Z,EnvIn2,EnvOut).
 
-evalAssign(t_assign(X,Y), EnvIn, EnvOut) :- 
-    evalIdentifier(X, _, IdName, EnvIn, EnvIn2),
-    evalBoolExpression(Y, Output, EnvIn2, EnvIn3),
-    update(IdName, Output, EnvIn3, EnvOut).
+% assignment
+evalAssign(t_assign(X,Y),EnvIn,EnvOut):-
+    evalIdentifier(X,_,EnvIn,EnvIn),evalExpression(Y,Output,EnvIn,EnvIn),
+    update(X,Output, EnvIn,EnvOut).
 
-evalNum(t_num(X), Output, EnvIn, EnvIn) :- Output = X, !.
-evalNumneg(t_numneg(X), Output, EnvIn, EnvIn) :- Output = X, !.
-evalIdentifier(t_identifier(X), Output, Ident, EnvIn, EnvIn) :- lookup(X, EnvIn, Output), Ident = X.
+evalAssign(t_assign(X,Y),EnvIn,EnvOut):-
+    evalIdentifier(X,_,EnvIn,EnvIn),evalBoolexp(Y,Output,EnvIn,EnvIn),
+    update(X,Output, EnvIn,EnvOut).
 
-evalDatatype(t_datatype(var), EnvIn, EnvIn).
-evalDatatype(t_datatype(bool), EnvIn, EnvIn).
-
-% Rules to evaluate expressions.
-
-evalExpression(t_add(X,Y), Output, EnvIn, EnvOut) :- evalTerm(X, TermOut, EnvIn, EnvIn2),
-                                                     evalExpression(Y, ExpOut, EnvIn2, EnvOut),
-                             atom_string(TermOut, QtermOut),
-                             atom_number(QtermOut, NtermOut),
-                             atom_string(ExpOut, QexpOut),
-                             atom_number(QexpOut, NexpOut),
-                                                     Output is NtermOut + NexpOut.
-
-evalExpression(t_sub(X,Y), Output, EnvIn, EnvOut) :- evalTerm(X, TermOut, EnvIn, EnvIn2),
-                                                     evalExpression(Y, ExpOut, EnvIn2, EnvOut),
-                             atom_string(TermOut, QtermOut),
-                             atom_number(QtermOut, NtermOut),
-                             atom_string(ExpOut, QexpOut),
-                             atom_number(QexpOut, NexpOut),
-                                                     Output is NtermOut - NexpOut.
-
-evalExpression(t_exp(X), Output, EnvIn, EnvOut) :- evalTerm(X, Output, EnvIn, EnvOut).
-
-evalTerm(t_mul(X,Y), Output, EnvIn, EnvOut) :- evalTerm(X, TermOut, EnvIn, EnvIn2),
-                                                     evalExpression(Y, ExpOut, EnvIn2, EnvOut),
-                             atom_string(TermOut, QtermOut),
-                             atom_number(QtermOut, NtermOut),
-                             atom_string(ExpOut, QexpOut),
-                             atom_number(QexpOut, NexpOut),
-                                               Output is NfactOut * NtermOut.
-
-evalTerm(t_div(X,Y), Output, EnvIn, EnvOut) :- evalTerm(X, TermOut, EnvIn, EnvIn2),
-                                                     evalExpression(Y, ExpOut, EnvIn2, EnvOut),
-                             atom_string(TermOut, QtermOut),
-                             atom_number(QtermOut, NtermOut),
-                             atom_string(ExpOut, QexpOut),
-                             atom_number(QexpOut, NexpOut),
-                                               Output is NfactOut / NtermOut.
-
-evalTerm(t_mod(X,Y), Output, EnvIn, EnvOut) :- evalTerm(X, TermOut, EnvIn, EnvIn2),
-                                                     evalExpression(Y, ExpOut, EnvIn2, EnvOut),
-                             atom_string(TermOut, QtermOut),
-                             atom_number(QtermOut, NtermOut),
-                             atom_string(ExpOut, QexpOut),
-                             atom_number(QexpOut, NexpOut),
-                                               Output is NfactOut mod NtermOut.
-
-evalTerm(t_term(X), Output, EnvIn, EnvOut) :- evalNum(X, Output, EnvIn, EnvOut).
-evalTerm(t_term(X), Output, EnvIn, EnvOut) :- evalNumneg(X, Output, EnvIn, EnvOut).
-evalTerm(t_term(X), Output, EnvIn, EnvOut) :- evalIdentifier(X, Output, EnvIn, EnvOut).
-
-% rule for control
-evalControl(t_control(X,Y,Z), EnvIn,EnvOut):- (evalCondition(X, EnvIn, EnvIn1)->  evalProcess(Y, EnvIn1, EnvOut));
-    								evalProcess(Z,EnvIn,EnvOut).
-
-% rule for iterate
-evalIterate(t_iterate(X,Y), EnvIn,EnvOut):- (evalCondition(X, EnvIn, EnvIn1)->  evalProcess(Y, EnvIn1, EnvIn2),evalIterate(t_iterate(X,Y), EnvIn2,EnvOut));EnvOut = EnvIn.
-
-%rule for condition 
-evalCondition(t_cond_and(X,Y),Output, EnvIn,EnvOut):- evalBoolexp(X,Output, EnvIn,EnvIn1),evalBoolexp(Y, Output, EnvIn1, EnvOut).
-evalCondition(t_cond_or(X,Y),Output, EnvIn,EnvOut):- evalBoolexp(X,Output, EnvIn,EnvIn1),evalBoolexp(Y, Output, EnvIn1, EnvOut).
-evalCondtion(t_cond_not(X),Output, EnvIn,EnvOut):- evalBoolexp(X,Output, EnvIn,EnvOut).
-evalCondition(t_cond(X),Output, EnvIn,EnvOut):- evalBoolexp(X,Output, EnvIn,EnvOut).
+%expressions
+evalExpression(t_expr(X),Output,EnvIn,EnvIn):-
+    evalTerm(X,Output,EnvIn,EnvIn).
 
 
+evalExpression(t_add(X,Y),Output,EnvIn,EnvIn):- 
+    evalTerm(X,Output1,EnvIn,EnvIn),
+    evalExpression(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 + Output2,!.
 
+evalExpression(t_sub(X,Y),Output,EnvIn,EnvIn):- 
+    evalTerm(X,Output1,EnvIn,EnvIn),
+    evalExpression(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 - Output2,!.
+
+%terms
+evalTerm(t_term(X),Output,EnvIn,EnvIn):-
+    evalNum(X,Output,EnvIn,EnvIn);
+    evalNumneg(X,Output,EnvIn,EnvIn),!.
+
+evalTerm(t_term(X),Output,EnvIn,EnvIn):-evalIdentifier(X,Output,EnvIn,EnvIn).
+
+evalTerm(t_mul(X,Y),Output,EnvIn,EnvIn):-
+    evalIdentifier(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 * Output2,!.
+
+evalTerm(t_div(X,Y),Output,EnvIn,EnvIn):-
+    evalIdentifier(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 / Output2,!.
+
+evalTerm(t_mod(X,Y),Output,EnvIn,EnvIn):-
+    evalIdentifier(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is mod(Output1,Output2),!.
+
+evalTerm(t_mul(X,Y),Output,EnvIn,EnvIn):-
+    evalNum(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 * Output2,!.
+
+evalTerm(t_div(X,Y),Output,EnvIn,EnvIn):-
+    evalNum(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 / Output2,!.
+
+evalTerm(t_mod(X,Y),Output,EnvIn,EnvIn):-
+    evalNum(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is mod(Output1,Output2),!.
+
+evalTerm(t_mul(X,Y),Output,EnvIn,EnvIn):-
+    evalNumneg(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 * Output2,!.
+
+evalTerm(t_div(X,Y),Output,EnvIn,EnvIn):-
+    evalNumneg(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is Output1 / Output2,!.
+
+evalTerm(t_mod(X,Y),Output,EnvIn,EnvIn):-
+    evalNumneg(X,Output1,EnvIn,EnvIn),
+    evalTerm(Y,Output2,EnvIn,EnvIn),
+    Output is mod(Output1,Output2),!.
+
+%number , neg number and identifier done
+evalNum(t_numb(X),Output,EnvIn,EnvIn):-Output is X,!.
+evalIdentifier(t_identifier(X),Output,EnvIn,_):-lookup(X, EnvIn, Output),!.
+evalNumneg(t_numbneg(X),Output,EnvIn,EnvIn):-evalNum(X,Output1,EnvIn,EnvIn),Output is 0-Output1,!.
