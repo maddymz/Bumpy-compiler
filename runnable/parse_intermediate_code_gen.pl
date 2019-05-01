f216531284d940ed560aa57403e83b9ccb9eead3
Bharat Goel <bgoel1@asu.edu>
2019-04-30 20:07:02 -0700

    Cleaned Code in parser

--- runnable/parse_intermediate_code_gen.pl
+++ runnable/parse_intermediate_code_gen.pl
@@ -1,23 +1,23 @@
 compileBumpy(FileName) :- open(FileName, read, InStream),
-		   tokenCodes(InStream, TokenCodes),
-		   phrase(lexer(Tokens),TokenCodes),
-		   parser(ParseTree, Tokens, []),
-		   close(InStream),
-		   split_string(FileName, ".", "", L),
-		   L = [H|_T],
-		   atom_concat(H, ".bmic", X),
-		   open(X, write, OutStream),
-		   write(OutStream, ParseTree),
-		   write(OutStream, '.'),
-close(OutStream).
+		      tokenCodes(InStream, TokenCodes),
+		       phrase(lexer(Tokens), TokenCodes),
+		      parser(ParseTree, Tokens, []),
+		      close(InStream),
+		      split_string(FileName, ".", "", L),
+		      L = [H|_T],
+		      atom_concat(H, ".bmic", X),
+		      open(X, write, OutStream),
+		      write(OutStream, ParseTree),
+		      write(OutStream, '.'),
+              close(OutStream).
 
 
-tokenCodes(InStream,[]) :- at_end_of_stream(InStream), !.
-tokenCodes(InStream, [TokenCode | RemTokens]) :- get_code(InStream, TokenCode),
-                                                 tokenCodes(InStream, RemTokens).
+tokenCodes(InStream, []) :- at_end_of_stream(InStream), !.
+tokenCodes(InStream, [TokenCode|RemTokens]) :- get_code(InStream, TokenCode), tokenCodes(InStream, RemTokens).
 
 
 %===========================================================================================
+%Tokenizer implemented with the list of keywords and other values
 
 lexer(Tokens) -->
     white_space,
@@ -61,99 +61,136 @@ lexer(Tokens) -->
         { Tokens = [Token | TokList] },lexer(TokList);  
     	  [],{ Tokens = [] }).
 
-white_space --> [Char], { code_type(Char,space) }, !, white_space.
+white_space --> [Char], {code_type(Char,space) }, !, white_space.
 white_space --> [].
 
-digit(Digit) --> [Digit],{ code_type(Digit, digit) }.
-digits([Digit|DigitTail]) --> digit(Digit),!,digits(DigitTail).
-digits([]) -->[].
+digit(Digit) --> [Digit], {code_type(Digit, digit) }.
+digits([Digit|DigitTail]) --> digit(Digit), !, digits(DigitTail).
+digits([]) --> [].
 
-number(Digit, Number) --> digits(DigitRem),{ number_chars(Number, [Digit|DigitRem]) }.
+number(Digit, Number) --> digits(DigitRem), {number_chars(Number, [Digit|DigitRem])}.
 
-upletter(Letter) -->[Letter], { code_type(Letter, upper) }.
+upletter(Letter) --> [Letter], { code_type(Letter, upper)}.
 
-lowletter(Letter) -->[Letter], { code_type(Letter, lower) }.
+lowletter(Letter) --> [Letter], { code_type(Letter, lower)}.
 
-alphanum([Alphanum|AlphaTail]) -->[Alphanum], { code_type(Alphanum, csym) }, !, alphanum(AlphaTail).
-alphanum([]) -->[].
+alphanum([Alphanum|AlphaTail]) --> [Alphanum], {code_type(Alphanum, csym)}, !, alphanum(AlphaTail).
+alphanum([]) --> [].
 
-identifier(IdentList, Ident) -->alphanum(IdentTail),{ atom_codes(Ident, [IdentList|IdentTail]) }.
+identifier(IdentList, Ident) --> alphanum(IdentTail), {atom_codes(Ident, [IdentList|IdentTail])}.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Initializing Parser, Takes tokens from lexer and generates parse tree 
+
 parser(ProgramNode) --> program(ProgramNode).
+program(t_program(CommentNode,BlockNode)) --> comment(CommentNode), block(BlockNode).
+program(t_program(BlockNode)) --> block(BlockNode).
 
-program(t_program(CommentNode,BlockNode)) -->comment(CommentNode),block(BlockNode).
-program(t_program(BlockNode)) -->block(BlockNode).
+%Rule for comment 
+comment(t_comment(WordNode)) --> [@], words(WordNode), [@].
 
-comment(t_comment(WordNode)) --> [@],words(WordNode),[@].
+%Rules for comment content
+words(t_words(IdentifierNode, WordNode)) --> identifier(IdentifierNode), words(WordNode).
+words(t_words(NumberNode, WordNode)) --> numb(NumberNode), words(WordNode).
+words(t_words(IdentifierNode)) --> identifier(IdentifierNode).
+words(t_words(NumberNode)) --> numb(NumberNode).
 
-words(t_words(IdentNumNode,WordNode)) --> identifier(IdentNumNode), words(WordNode) ; numb(IdentNumNode), words(WordNode).
-words(t_words(IdentNumNode)) -->identifier(IdentNumNode); numb(IdentNumNode).
+%Rule for Block
+block(t_block(DeclareNode, ProcessNode)) --> [start], declaration(DeclareNode), process(ProcessNode), [stop].
 
-block(t_block(DeclareNode,ProcessNode)) --> [start], declaration(DeclareNode), process(ProcessNode), [stop].
-
+%Rules for datatypes
 datatype(t_datatype(var)) --> [var].
 datatype(t_datatype(bool)) --> [bool].
 
-declaration(t_declare(DataTypeNode,IdentifierNode,DeclareNode)) --> datatype(DataTypeNode), identifier(IdentifierNode), [;], declaration(DeclareNode).    
-declaration(t_declare(DataTypeNode,IdentifierNode)) -->datatype(DataTypeNode),identifier(IdentifierNode),[;].
+%Rules for declaration
+declaration(t_declare(DataTypeNode, IdentifierNode, DeclareNode)) --> datatype(DataTypeNode), identifier(IdentifierNode), [;], declaration(DeclareNode).    
+declaration(t_declare(DataTypeNode, IdentifierNode)) --> datatype(DataTypeNode), identifier(IdentifierNode), [;].
 
-process(t_process(ConstructNode,ProcessNode)) --> assignvalue(ConstructNode), [;], process(ProcessNode); control(ConstructNode), process(ProcessNode); iterate(ConstructNode), process(ProcessNode); print(ConstructNode), process(ProcessNode);
-                            readValue(ConstructNode), process(ProcessNode).
-process(t_process(ConstructNode)) -->assignvalue(ConstructNode),[;] ;control(ConstructNode) ;iterate(ConstructNode);print(ConstructNode);readValue(ConstructNode).
+%Rules for process
+process(t_process(AssignNode, ProcessNode)) --> assignvalue(AssignNode), process(ProcessNode).
+process(t_process(ControlNode, ProcessNode)) --> control(ControlNode), process(ProcessNode).
+process(t_process(LoopNode, ProcessNode)) --> iterate(LoopNode), process(ProcessNode).
+process(t_process(PrintNode, ProcessNode)) --> print(PrintNode), process(ProcessNode).
+process(t_process(ReadNode, ProcessNode)) --> readValue(ReadNode), process(ProcessNode).
+process(t_process(AssignNode)) --> assignvalue(AssignNode).
+process(t_process(ControlNode)) --> control(ControlNode).
+process(t_process(LoopNode)) --> iterate(LoopNode).
+process(t_process(PrintNode)) --> print(PrintNode).
+process(t_process(ReadNode)) --> readValue(ReadNode).
 
+%Rule for reading values
 readValue(t_read(IdentifierNode)) --> [input], identifier(IdentifierNode), [;].
 
-print(t_print(ExpressionNode)) --> [show],expression(ExpressionNode),[;].
+%Rules for printing values
+print(t_print(ExpressionNode)) --> [show], expression(ExpressionNode),[;].
 print(t_printString(ValueNode)) --> [show], [*], value(ValueNode), [*], [;], !.
 
-value(ValueNode,ListNode,RemListNode):-stringCreate(PrintListNode,ListNode,RemListNode),atomic_list_concat(PrintListNode," ",ValueNode).
+value(ValueNode, ListNode, RemListNode) :- stringCreate(PrintListNode, ListNode, RemListNode), atomic_list_concat(PrintListNode," ", ValueNode).
 
 stringCreate(PrintListNode, ListNode, RemListNode) :- 
     ListNode = [Head|Tail],
     Head \= '*',
-    stringCreate(Final,Tail,RemListNode),
+    stringCreate(Final, Tail, RemListNode),
     PrintListNode = [Head|Final].
-stringCreate([],ListNode,ListNode) :- ListNode = [Head|_], Head = '*'.
+stringCreate([], ListNode, ListNode) :- ListNode = [Head|_], Head = '*'.
 
-assignvalue(t_assign(IdentifierNode,ExpressionNode)) --> identifier(IdentifierNode), [=] ,expression(ExpressionNode); identifier(IdentifierNode), [is], boolexp(ExpressionNode).
+%Rules for assigning values
+assignvalue(t_assign(IdentifierNode, ExpressionNode)) --> identifier(IdentifierNode), [=] ,expression(ExpressionNode), [;].
+assignvalue(t_assign(IdentifierNode, BooleanExpressionNode)) -->  identifier(IdentifierNode), [is], boolexp(BooleanExpressionNode), [;].
 
-control(t_control(ConditionNode,ProcessNode,ProcessNode1)) --> [incase], condition(ConditionNode), [do], process(ProcessNode), [otherwise], process(ProcessNode1), [endcase].
+%Rules for control structure
+control(t_control(ConditionNode, TrueProcessNode, FalseProcessNode)) --> [incase], condition(ConditionNode), 
+                                                                        [do], process(TrueProcessNode), [otherwise],
+                                                                        process(FalseProcessNode), [endcase].
 
-iterate(t_iterate(ConditionNode,ProcessNode)) --> [when], condition(ConditionNode), [repeat], process(ProcessNode), [endrepeat].
+%Rules for loop structure
+iterate(t_iterate(ConditionNode, ProcessNode)) --> [when], condition(ConditionNode), [repeat], process(ProcessNode), [endrepeat].
 
-condition(t_cond_and(ExpressionNode,ExpressionNode1)) --> boolexp(ExpressionNode), [and], boolexp(ExpressionNode1).
-condition(t_cond_or(ExpressionNode,ExpressionNode1)) --> boolexp(ExpressionNode), [or], boolexp(ExpressionNode1).
-condition(t_cond_not(ExpressionNode)) --> [~], boolexp(ExpressionNode).
-condition(t_cond(ExpressionNode)) --> boolexp(ExpressionNode).
+%Rules for conditions
+condition(t_cond_and(BooleanExpressionNode1, BooleanExpressionNode2)) --> boolexp(BooleanExpressionNode1), [and], boolexp(BooleanExpressionNode2).
+condition(t_cond_or(BooleanExpressionNode1, BooleanExpressionNode2)) --> boolexp(BooleanExpressionNode1), [or], boolexp(BooleanExpressionNode2).
+condition(t_cond_not(BooleanExpressionNode)) --> [~], boolexp(BooleanExpressionNode).
+condition(t_cond(BooleanExpressionNode)) --> boolexp(BooleanExpressionNode).
 
-boolexp(t_boolexp_eq(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [:=:], expression(ExpressionNode1).
-boolexp(t_boolexp_neq(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [~=], expression(ExpressionNode1). 
-boolexp(t_boolexp_leq(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [<],[=], expression(ExpressionNode1).
-boolexp(t_boolexp_geq(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [>],[=], expression(ExpressionNode1).
-boolexp(t_boolexp_less(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [<], expression(ExpressionNode1).
-boolexp(t_boolexp_great(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [>], expression(ExpressionNode1).
-boolexp(t_boolexp_beq(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [:=:], boolexp(ExpressionNode1).
-boolexp(t_boolexp_bneq(ExpressionNode,ExpressionNode1)) --> expression(ExpressionNode), [~=], boolexp(ExpressionNode1). 
+%Rules for Boolean expressions 
+boolexp(t_boolexp_eq(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [:=:], expression(ExpressionNode2).
+boolexp(t_boolexp_neq(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [~=], expression(ExpressionNode2). 
+boolexp(t_boolexp_leq(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [<],[=], expression(ExpressionNode2).
+boolexp(t_boolexp_geq(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [>],[=], expression(ExpressionNode2).
+boolexp(t_boolexp_less(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [<], expression(ExpressionNode2).
+boolexp(t_boolexp_great(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [>], expression(ExpressionNode2).
+boolexp(t_boolexp_beq(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [:=:], boolexp(ExpressionNode2).
+boolexp(t_boolexp_bneq(ExpressionNode1, ExpressionNode2)) --> expression(ExpressionNode1), [~=], boolexp(ExpressionNode2). 
 boolexp(t_boolexp(yes)) --> [yes].
 boolexp(t_boolexp(no)) --> [no].
 
-expression(t_add(TermNode,ExpressionNode)) --> term(TermNode),[+],expression(ExpressionNode).
-expression(t_sub(TermNode,ExpressionNode)) --> term(TermNode),[-],expression(ExpressionNode).
+%Rules for expressions
+expression(t_add(TermNode, ExpressionNode)) --> term(TermNode), [+], expression(ExpressionNode).
+expression(t_sub(TermNode, ExpressionNode)) --> term(TermNode), [-], expression(ExpressionNode).
 expression(t_expr(TermNode)) --> term(TermNode).
 
-term(t_mul(IdentNumNode,TermNode)) --> identifier(IdentNumNode),[*],term(TermNode);numb(IdentNumNode),[*],term(TermNode);numbneg(IdentNumNode),[*],term(TermNode).
-term(t_div(IdentNumNode,TermNode)) -->identifier(IdentNumNode),[/],term(TermNode);numb(IdentNumNode),[/],term(TermNode);numbneg(IdentNumNode),[/],term(TermNode).
-term(t_mod(IdentNumNode,TermNode)) -->identifier(IdentNumNode),[mod],term(TermNode);numb(IdentNumNode),[mod],term(TermNode);numbneg(IdentNumNode),[mod],term(TermNode).
-term(t_term(IdentNumNode)) -->identifier(IdentNumNode);numb(IdentNumNode);numbneg(IdentNumNode).
+term(t_mul(IdentifierNode, TermNode)) --> identifier(IdentifierNode), [*], term(TermNode).
+term(t_mul(NumberNode, TermNode)) --> numb(NumberNode),[*], term(TermNode).
+term(t_mul(NegNumberNode, TermNode)) --> numbneg(NegNumberNode), [*], term(TermNode).
+term(t_div(IdentifierNode, TermNode)) --> identifier(IdentifierNode), [/], term(TermNode).
+term(t_mul(NumberNode, TermNode)) --> numb(NumberNode), [/], term(TermNode).
+term(t_mul(NegNumberNode, TermNode)) --> numbneg(NegNumberNode), [/], term(TermNode).
+term(t_mod(IdentifierNode, TermNode)) -->identifier(IdentifierNode), [mod], term(TermNode).
+term(t_mul(NumberNode, TermNode)) --> numb(NumberNode), [mod], term(TermNode).
+term(t_mul(NegNumberNode, TermNode)) --> numbneg(NegNumberNode), [mod], term(TermNode).
+term(t_term(IdentifierNode)) --> identifier(IdentifierNode).
+term(t_term(NumberNode)) --> numb(NumberNode).
+term(t_term(NegNumberNode)) --> numbneg(NegNumberNode).
 
-identifier(t_identifier(IdentifierNode)) -->[IdentifierNode], 
-    {string_chars(IdentifierNode,[IdentHead|IdentTail])}, 
+%Rule for Identifier
+identifier(t_identifier(IdentifierNode)) --> [IdentifierNode], 
+    {string_chars(IdentifierNode, [IdentHead|IdentTail])}, 
     {(is_alpha(IdentHead); IdentifierNode = '_')},
-    {forall(member(Char,IdentTail),
+    {forall(member(Char, IdentTail),
     (is_alnum(Char)); Char = '_')}.
 
-numbneg(t_numbneg(NumNode)) --> [-],numb(NumNode).
-numb(t_numb(NumNode)) --> [NumNode],{number(NumNode)}.
+%Rules for numbers
+numbneg(t_numbneg(NumberNode)) --> [-],numb(NumberNode).
+numb(t_numb(NumberNode)) --> [NumberNode],{number(NumberNode)}.
